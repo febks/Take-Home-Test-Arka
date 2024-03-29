aa3352dc59d6fd2175056f9acdc914dc2a28cc0d
Febriana Kusumah <kusumahfebriana@gmail.com>
2024-03-29 14:57:43 +0700

    done number 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Layout } from "antd";
 import { ROUTES } from "./constant/routesConstant";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Number1 from './pages/number1/Number1';
+import Number2 from './pages/number2/Number2';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path={ROUTES.NUMBER1} element={<Number1 />} exact />
+            <Route path={ROUTES.NUMBER2} element={<Number2 />} exact />
           </Routes>
         </BrowserRouter>
       </Layout>
---
+++ src/constant/arrayConstant.js
@@ -0,0 +1,82 @@
+export const jenjangs = [
+  {
+    id: 1,
+    jenjang: "Pendidikan Pra Sekolah",
+    tingkatan: [
+      {
+        id: 11,
+        tingkatan: "PAUD (Pendidikan Anak Usia Dini)",
+      },
+      {
+        id: 12,
+        tingkatan: "TK (Taman Kanak-Kanak",
+      },
+      {
+        id: 13,
+        tingkatan: "RA (Rahdatul Athfal)",
+      }
+    ]
+  },
+  {
+    id: 2,
+    jenjang: "Pendidikan Dasar",
+    tingkatan: [
+      {
+        id: 21,
+        tingkatan: "SD (Sekolah Dasar)",
+      },
+      {
+        id: 22,
+        tingkatan: "MI (Madrasah Ibtidaiyah)",
+      },
+      {
+        id: 23,
+        tingkatan: "SMP (Sekolah Menengah Pertama)",
+      },
+      {
+        id: 24,
+        tingkatan: "MTs (Madrasah Tsanawiyah)",
+      },
+    ]
+  },
+  {
+    id: 3,
+    jenjang: "Pendidikan Menengah",
+    tingkatan: [
+      {
+        id: 31,
+        tingkatan: "SMA (Sekolah Menengah Atas)",
+      },
+      {
+        id: 32,
+        tingkatan: "MA (Madrasah Aliyah)",
+      },
+      {
+        id: 33,
+        tingkatan: "SMK (Sekolah Menengah Kejuruan)",
+      },
+    ]
+  },
+  {
+    id: 4,
+    jenjang: "Pendidikan Tinggi",
+    tingkatan: [
+      {
+        id: 41,
+        tingkatan: "D3 Diploma",
+      },
+      {
+        id: 42,
+        tingkatan: "S1/D4 Sarjana",
+      },
+      {
+        id: 43,
+        tingkatan: "S2 Magister",
+      },
+      {
+        id: 44,
+        tingkatan: "S3 Doktoral",
+      },
+    ]
+  },
+]
