3c321f30a5387724816bbcd5a88890f4d71e1388
Febriana Kusumah <kusumahfebriana@gmail.com>
2024-03-29 12:58:57 +0700

    install packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { Layout } from "antd";
+import { Routes, Route } from "react-router-dom";
+import { ROUTES } from "./constant/routesConstant";
+import Number1 from './pages/number1/Number1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Layout>
+        <Routes>
+          <Route path={ROUTES.NUMBER1} element={<Number1 />} exact />
+        </Routes>
+      </Layout>
+    </Fragment>
   );
 }
 
---
+++ src/constant/routesConstant.js
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  NUMBER1: "/",
+  NUMBER2: "/number2",
+  NUMBER3: "/number3",
+}
